881fe2e4a61aa142463c23cca67b9160e285d3c2
0testya <120251989+0testya@users.noreply.github.com>
2022-12-10 23:29:08 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,139 @@
+//Set of questions and answers -DONE
+//Give each answer an identifier -DONE
+//Each identifier will increment through each question
+//St the end the identifier with the highest number is the winner 
+//Display the answer and rest the quiz
+
+//pass results frm previous question to the next page usig localcache
+
+//Randomise the background of the quiz for each questiion
+
+//Possible - Personality Traits
+// 15 -21- You Need Help
+// 10 - 15 - Good Soul
+// 5- 10 - Meh 
+// 5 - Are You Even Real
+
+
+let currentQuestion = 0;
+let score = [];
+let selectedAnswersData = [];
+const totalQuestions =questions.length;
+
+const container = document.querySelector('.quiz-container');
+const questionEl = document.querySelector('.question');
+const option1 = document.querySelector('.option1');
+const option2 = document.querySelector('.option2');
+const option3 = document.querySelector('.option3');
+const nextButton = document.querySelector('.next');
+const previousButton = document.querySelector('.previous');
+const restartButton = document.querySelector('.restart');
+const result = document.querySelector('.result');
+
+//Function to generate question 
+function generateQuestions (index) {
+    //Select each question by passing it a particular index
+    const question = questions[index];
+    const option1Total = questions[index].answer1Total;
+    const option2Total = questions[index].answer2Total;
+    const option3Total = questions[index].answer3Total;
+    //Populate html elements 
+    questionEl.innerHTML = `${index + 1}. ${question.question}`
+    option1.setAttribute('data-total', `${option1Total}`);
+    option2.setAttribute('data-total', `${option2Total}`);
+    option3.setAttribute('data-total', `${option3Total}`);
+    option1.innerHTML = `${question.answer1}`
+    option2.innerHTML = `${question.answer2}`
+    option3.innerHTML = `${question.answer3}`
+}
+
+
+function loadNextQuestion () {
+    const selectedOption = document.querySelector('input[type="radio"]:checked');
+    //Check if there is a radio input checked
+    if(!selectedOption) {
+        alert('Please select your answer!');
+        return;
+    }
+    //Get value of selected radio
+    const answerScore = Number(selectedOption.nextElementSibling.getAttribute('data-total'));
+
+    ////Add the answer score to the score array
+    score.push(answerScore);
+
+    selectedAnswersData.push()
+    
+
+    const totalScore = score.reduce((total, currentNum) => total + currentNum);
+
+    //Finally we incement the current question number ( to be used as the index for each array)
+    currentQuestion++;
+
+        //once finished clear checked
+        selectedOption.checked = false;
+    //If quiz is on the final question
+    if(currentQuestion == totalQuestions - 1) {
+        nextButton.textContent = 'Finish';
+    }
+    //If the quiz is finished then we hide the questions container and show the results 
+    if(currentQuestion == totalQuestions) {
+        container.style.display = 'none';
+        result.innerHTML =
+         `<h1 class="final-score">Your score: ${totalScore}</h1>
+         <div class="summary">
+            <h1>Summary</h1>
+            <h3>see below for a summary based on your results:</h3>
+            <h3>15 - 21- you are 100% evil💀</h3>
+            <h3>10 - 15 - you are 50% evil👹</h3>
+            <h3>5 - 10 - you are not evil🥳 </h3>
+            <h3>5 - you are a good person😇</h3>
+        </div>
+        <button class="restart">Restart Quiz</button>
+         `;
+        return;
+    }
+    generateQuestions(currentQuestion);
+}
+
+//Function to load previous question
+function loadPreviousQuestion() {
+    //Decrement quentions index
+    currentQuestion--;
+    //remove last array value;
+    score.pop();
+    //Generate the question
+    generateQuestions(currentQuestion);
+}
+
+//Fuction to reset and restart the quiz;
+function restartQuiz(e) {
+    if(e.target.matches('button')) {
+    //reset array index and score
+    currentQuestion = 0;
+    score = [];
+    //Reload quiz to the start
+    location.reload();
+    }
+
+}
+
+
+generateQuestions(currentQuestion);
+nextButton.addEventListener('click', loadNextQuestion);
+previousButton.addEventListener('click',loadPreviousQuestion);
+result.addEventListener('click',restartQuiz);
+
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        1000
+    )
+});
+
+
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
---
+++ questions.js
@@ -0,0 +1,69 @@
+const questions = [
+  {
+    "question": "do you feel sincere with your lies?",
+    "answer1": "no",
+    "answer1Total": "1",
+    "answer2": "sometimes",
+    "answer2Total": "2",
+    "answer3": "yes",
+    "answer3Total": "3"
+  },
+  {
+    "question": "do you withholds information to get things done.",
+    "answer1": "no",
+    "answer1Total": "1",
+    "answer2": "sometimes",
+    "answer2Total": "2",
+    "answer3": "yes",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "Are you a negative person",
+    "answer1": "no",
+    "answer1Total": "1",
+    "answer2": "yes",
+    "answer2Total": "3",
+    "answer3": "a little bit",
+    "answer3Total": "2"
+  },
+  {
+    "question": "do you run from your responsibilities?",
+    "answer1": "all the time.",
+    "answer1Total": "3",
+    "answer2": "sometimes.",
+    "answer2Total": "2",
+    "answer3":
+      "never",
+    "answer3Total": "1"
+  },
+  {
+    "question": "You are a dishonest partner, friend, and team player",
+    "answer1": "no",
+    "answer1Total": "1",
+    "answer2": "not really",
+    "answer2Total": "2",
+    "answer3": "yes",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "You are an insecure, jealous, and toxic person",
+    "answer1":
+      "yes",
+    "answer1Total": "3",
+    "answer2": "i feel jealous sometimes",
+    "answer2Total": "2",
+    "answer3": "no",
+    "answer3Total": "1"
+  },
+  {
+    "question": "you don't feel people pain",
+    "answer1": "no",
+    "answer1Total": "1",
+    "answer2": "I feel a little bit of peole pain",
+    "answer2Total": "2",
+    "answer3": "yes",
+    "answer3Total": "3"
+  }
+]
